9b14c9c2d464fcd7b57374e769b5a1dfaa80be76
xhesildamyrta <75999512+xhesildamyrta@users.noreply.github.com>
2021-11-27 21:06:26 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,7 @@
         <div class="row">
             <div class="col-12 text-center pt-5">
                 <h1 class="display-one mt-5">{{ config('app.name') }}</h1>
-                <p>This awesome blog has many articles, click the button below to see them</p>
-                <br>
+                <p>This awesome blog has many articles, click the button below to see them</p><br>
                 <a href="/blog" class="btn btn-outline-primary">Show Blog</a>
             </div>
         </div>
